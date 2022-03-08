a56452e12931f7c5da109d415b7d0403c0b8d5e9
devops-tom <thomas2.moran@outlook.com>
2022-03-08 19:35:10 +0000

    Add keypair

--- aws/compute.tf
+++ aws/compute.tf
@@ -1,3 +1,13 @@
+resource "tls_private_key" "private_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = "ssh-keypair"
+  public_key = tls_private_key.private_key.public_key_openssh
+}
+
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 3.0"
@@ -6,6 +16,7 @@ module "ec2_instance" {
 
   ami                    = "ami-0f5513ad02f8d23ed"
   instance_type          = "t2.micro"
+  key_name               = aws_key_pair.generated_key.key_name
 
   tags = {
     Terraform   = "true"
